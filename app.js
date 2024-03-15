e92351dd6084b4ab877fe74f062646f9abb320e8
Jun Zhang <jzhang213@sheffield.ac.uk>
2024-03-15 18:17:41 +0000

    change route from index to add, rebuild index, fix routes

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
-var addRouter = require('./routes/add');
 var usersRouter = require('./routes/posts');
+var addRouter = require('./routes/add');
+var displayRouter = require('./routes/display');
 
 var app = express();
 
@@ -25,7 +26,8 @@ app.use('/public/images/uploads', express.static(path.join(__dirname, '/public/i
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/add', addRouter);
+app.use('/', addRouter);
+app.use('/',displayRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/display.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+var plantsightings = require('../controllers/plantsightings')
+var multer = require('multer');
+
+// storage defines the storage options to be used for file upload with multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/uploads/');
+    },
+    filename: function (req, file, cb) {
+        var original = file.originalname;
+        var file_extension = original.split(".");
+        // Make the file name the date + the file extension
+        filename =  Date.now() + '.' + file_extension[file_extension.length-1];
+        cb(null, filename);
+    }
+});
+let upload = multer({ storage: storage });
+
+router.get('/display', function(req, res, next) {
+    let result = plantsightings.getAll()
+    result.then(plantsightings => {
+        let data = JSON.parse(plantsightings);
+        // Convert dateSeen from string to Date object for each plantsighting
+        data = data.map(sighting => ({
+            ...sighting,
+            dateSeen: new Date(sighting.dateSeen)
+        }));
+
+        console.log(data.length)
+        res.render('display', { title: 'All PlantSights', data: data});
+    })
+});
+
+module.exports = router;
