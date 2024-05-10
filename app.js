a8caf1ec50f520c9665174631cbcdb89dd961838
Mengxiao Li <mli166@sheffield.ac.uk>
2024-05-10 21:53:35 +0100

    add geolocation, google map api, take photo

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/public/images/uploads', express.static(path.join(__dirname, '/public/images/uploads')));
-
+app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/', addRouter);
--- routes/add.js
+++ routes/add.js
@@ -23,17 +23,42 @@ router.get('/add', function(req, res, next) {
   res.render('add', { title: 'New PlantSight Form' });
 });
 
-router.post('/add', upload.single('myImg'), function (req, res, next) {
-  if (!req.file) {
-    console.error('No file uploaded with the request!');
-    return res.sendStatus(400); // Bad request error code
+// router.post('/add', upload.single('myImg'), function (req, res, next) {
+//   if (!req.file) {
+//     console.error('No file uploaded with the request!');
+//     return res.sendStatus(400); // Bad request error code
+//   }
+//
+//   let formData = req.body;
+//   let photoPath = req.file.path;
+//   let result = plantsightings.create(formData, photoPath);
+//   console.log(result);
+//   res.redirect('/add');
+// });
+router.post('/add', function (req, res, next) {
+  let photoData = req.body.photo;
+
+  if (!photoData) {
+    console.error('No photo data received!');
+    return res.sendStatus(400);
   }
 
-  let formData = req.body;
-  let photoPath = req.file.path;
-  let result = plantsightings.create(formData, photoPath);
-  console.log(result);
-  res.redirect('/add');
+  // Extract base64 part
+  const base64Data = photoData.replace(/^data:image\/\w+;base64,/, "");
+  const buffer = Buffer.from(base64Data, 'base64');
+
+  // Save the image
+  require('fs').writeFile('public/images/uploads/' + Date.now() + '.png', buffer, function(err) {
+    if (err) {
+      console.error('Error saving image!', err);
+      return res.sendStatus(500);
+    }
+    let formData = req.body;
+    let photoPath = 'images/uploads/' + Date.now() + '.png';
+    let result = plantsightings.create(formData, photoPath);
+    console.log(result);
+    res.redirect('/add');
+  });
 });
 
 module.exports = router;
