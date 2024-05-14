3c7ce82404b0b028a9843009d4facc81de22069f
James <jdcordrey1@sheffield.ac.uk>
2024-05-15 00:38:46 +0100

    restructured the display page with css and other stuff

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var request = indexedDB.open('MyDatabase', 2);  // Open the IndexedDB
 
-    request.onerror = function(event) {
+    request.onerror = function (event) {
         console.error("IndexedDB error:", event.target.errorCode);
     };
 
-    request.onsuccess = function(event) {
+    request.onsuccess = function (event) {
         var db = event.target.result;
         var tx = db.transaction('nicknames', 'readonly');
         var store = tx.objectStore('nicknames');
         var getRequest = store.get('userNickname');  // Get the 'userNickname' from the store
 
-        getRequest.onsuccess = function() {
+        getRequest.onsuccess = function () {
             var storedNickname = getRequest.result ? getRequest.result.nickname : '';
             var serverNickname = document.getElementById('serverNickname').textContent.trim();  // Read the server-side injected nickname
 
@@ -22,25 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
             if (localNickname !== serverNickname || localNickname === '') {
 
                 var buttonContainer = document.getElementById('buttonContainerSuggest');
-                var buttonHTML = `<button type="button" class="btn btn-primary" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#inputSuggestNameModal">Suggest a Name</button>`;
+                var buttonHTML = `<button type="button" class="btn btn-primary" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#inputSuggestNameModal">Suggest Identification</button>`;
                 buttonContainer.innerHTML = buttonHTML;  // Insert the button into the container
 
             } else {
 
                 console.log("should display button");
                 var buttonContainer = document.getElementById('buttonContainerChoose');
-                var buttonHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#suggestNameModal">Choose Suggested Names</button>`;
+                var buttonHTML = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#suggestNameModal">User Suggested Names</button>`;
                 buttonContainer.innerHTML = buttonHTML;  // Insert the button into the container
             }
 
         };
 
-        getRequest.onerror = function(event) {
+        getRequest.onerror = function (event) {
             console.error('Error fetching nickname from IndexedDB:', event.target.errorCode);
         };
 
-        tx.oncomplete = function() {
+        tx.oncomplete = function () {
             db.close();  // Close the db when the transaction is done
         };
     };
 });
+
+function openPopup(imageUrl) {
+    window.open(imageUrl, '_blank');
+}
