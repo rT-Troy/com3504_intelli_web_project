8bb8faa0aae280e5e13e1ff59365e7ac6d3c7c10
RamboSage <mmbayoo.com@gmail.com>
2024-05-14 16:27:16 +0100

    DBPedia Search thumbnail included with link to wiki page

--- routes/search-plants.js
+++ routes/search-plants.js
@@ -6,13 +6,15 @@ var axios = require('axios');
 router.get('/search-plants', function(req, res, next) {
     const query = req.query.query;
     const sparqlQuery = `
-        SELECT DISTINCT ?label ?description WHERE {
+        SELECT DISTINCT ?label ?description ?thumbnail ?link WHERE {
             ?plant a dbo:Plant ;
-                   rdfs:label ?label .
+                   rdfs:label ?label ;
+                   foaf:isPrimaryTopicOf ?link .
             OPTIONAL { ?plant dbo:abstract ?description }
+            OPTIONAL { ?plant dbo:thumbnail ?thumbnail }
             FILTER (langMatches(lang(?label), "EN") && regex(?label, "${query}", "i")).
             FILTER (langMatches(lang(?description), "EN"))
-        } LIMIT 3`;
+        } LIMIT 5`;
 
     const url = `https://dbpedia.org/sparql?query=${encodeURIComponent(sparqlQuery)}&format=json`;
 
@@ -20,7 +22,9 @@ router.get('/search-plants', function(req, res, next) {
         .then(response => {
             const plants = response.data.results.bindings.map(bind => ({
                 label: bind.label.value,
-                description: bind.description ? bind.description.value : null
+                description: bind.description ? bind.description.value.split(' ').slice(0, 20).join(' ') + '...' : 'No description available',
+                thumbnail: bind.thumbnail ? bind.thumbnail.value : null,
+                link: bind.link.value
             }));
             res.json(plants);
         })
